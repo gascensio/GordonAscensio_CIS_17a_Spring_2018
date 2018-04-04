432a07de5adad9ef34f9a4bd81f149fb3bea0f39
gascensio <gaascensio@gmail.com>
2018-04-04 10:37:43 -0700

    Add files via upload

--- HomeWork/Project1306/main.cpp
+++ HomeWork/Project1306/main.cpp
@@ -1,25 +1,45 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  Created on: April 1, 2018
+ *      Author: Gordon Ascensio
+ *       Class: CIS 17A
+ *   Professor: Mark Lehr
+ *        Days: MW Time: 12:45p - 2:30
+ * 	   Purpose: Demonstrates Inventory Class.
  */
-
-/* 
- * File:   main.cpp
- * Author: Gordon
- *
- * Created on April 4, 2018, 10:24 AM
- */
-
-#include <cstdlib>
+#include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+#include "Inventory.h"
 
-    return 0;
+void PrintItem(Inventory Item);
+
+int main()
+{
+	Inventory hat(1, 7.95, 50);
+	Inventory shirt(2, 9.95, 10);
+
+	//Initialize total cost member.
+	hat.setTotalCost();
+	shirt.setTotalCost();
+
+	//Print each item.
+	PrintItem(hat);
+	cout << endl;
+	PrintItem(shirt);
+
+}
+/**********************************************************
+ * Function PrintItem prints the fields of an Inventory
+ * Object.
+ */
+void PrintItem(Inventory Item)
+{
+	cout << "Item number #"<< Item.getItemNumber()
+			<< " quantity " <<  Item.getQuantity()
+			<< fixed << setprecision(2)
+			<< " cost $" << Item.getCost()
+			<< " Total cost $" << Item.getTotalCost();
 }
 

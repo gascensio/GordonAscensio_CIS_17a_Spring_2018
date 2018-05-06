f202463f40d66bd1289b08441fe8f708acd856de
gascensio <gaascensio@gmail.com>
2018-05-06 13:27:35 -0700

    Add files via upload

---
+++ HomeWork/Project1405/main.cpp
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Gordon
+ *
+ * Created on May 4, 2018, 2:37 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include "numdays.h"
+#include "timeoff.h"
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    
+    TimeOff Time("John Doe", 1);
+    
+    
+    return 0;
+}
+

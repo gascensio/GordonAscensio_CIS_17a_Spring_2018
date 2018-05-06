bf7f82b5369ebc5f3fe56ac341b16d0b4cf6f73d
gascensio <gaascensio@gmail.com>
2018-05-06 13:27:47 -0700

    Add files via upload

---
+++ HomeWork/Project1405/numdays.cpp
@@ -0,0 +1,92 @@
+
+
+/*
+ *  Created on: May 4, 2018
+ *      Author: Gordon Ascensio
+ *       Class: CIS 17A
+ *   Professor: Mark Lehr
+ *        Days: MW Time: 12:45p - 2:30
+ *     Purpose: Converts work hours to days
+ */
+
+#include <iostream>
+#include "numdays.h"
+using namespace std;
+
+NumDays::NumDays(int hr)
+{
+    if(hr > 0)
+        workhr = hr;
+    else 
+        workhr = 0;
+}
+NumDays NumDays::operator+(const NumDays &right)
+{
+    NumDays temp(this->workhr);
+    
+    temp.workhr = this->workhr + right.workhr;
+    if(temp.workhr < 0)
+        temp.workhr = 0;
+    return temp;
+}
+NumDays NumDays::operator++()
+{
+    ++workhr;
+    return *this;
+}
+NumDays NumDays::operator++(int)
+{
+    NumDays temp(workhr);
+    
+    workhr++;
+    return temp;
+}
+NumDays NumDays::operator-(const NumDays &right)
+{
+    NumDays temp(this->workhr);
+    
+    temp.workhr = this->workhr - right.workhr;
+    if(temp.workhr < 0)
+        temp.workhr = 0;
+    return temp;
+}
+NumDays NumDays::operator--()
+{
+    if(this->workhr < 1)
+        this->workhr = 0;
+    --workhr;
+    return *this;
+}
+NumDays NumDays::operator--(int)
+{
+    NumDays temp(workhr);
+    
+    if(this->workhr < 1)
+        this->workhr = 0;
+    else
+        this->workhr--;
+    return temp;
+    
+    
+}
+ void NumDays::sethours(int hr)
+ {
+     if(hr < 0){
+         cout << "ERROR";
+         exit(0);
+     }else 
+         this->workhr = hr;
+     return;
+ }
+ float NumDays::getworkdays()
+ {
+     float out = static_cast<float>(this->workhr) / 8.0;
+     return out;
+ }
+ void NumDays::setworkdays(float days)
+ {
+     if(days < 0)
+         this->workhr = 0;
+     else
+         this->workhr = days * 8;
+ }
